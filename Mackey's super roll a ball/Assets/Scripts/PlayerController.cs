add5996d001828ff27b1f4649ef2833f407621b2
mvcodeclub <sarah@mvcodeclub.com>
2018-09-18 17:52:13 -0700

    Started project
    Started Project

---
+++ Mackey's super roll a ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+		rb.AddForce(Vector3.forward * -10f);
+		
+	}
+}
