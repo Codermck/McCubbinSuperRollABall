8f2944b6504561675fd1f6202f82b004e261ee78
mvcodeclub <sarah@mvcodeclub.com>
2018-09-18 18:34:16 -0700

    Added vectors for hedghog movement uncomplete

--- Mackey's super roll a ball/Assets/Scripts/PlayerController.cs
+++ Mackey's super roll a ball/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public float moveSpeed = 1000f;
+
 	Rigidbody rb;
 
 	// Use this for initialization
@@ -13,7 +15,14 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {		
+	void Update () {
+		float hdir = Input.GetAxisRaw ("Horizontal");
+		float vdir = Input.GetAxisRaw ("Vertical");
+
+		Vector3 directionVector = new Vector3 (hdir, 0, vdir);
+		Vector3 unitVector = directionVector.normalized;
+		Vector3 forceVector = unitVector * moveSpeed + Time.deltaTime;
+
 		rb.AddForce(Vector3.forward * -10f);
 		
 	}
