cbd8838b4abe209defc4ca179c285183738f0d53
mvcodeclub <sarah@mvcodeclub.com>
2018-09-25 18:41:05 -0700

    added movement (arrow keys) and camera.

--- Mackey's super roll a ball/Assets/Scripts/PlayerController.cs
+++ Mackey's super roll a ball/Assets/Scripts/PlayerController.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed = 1000f;
-
+	Vector3 offset;
 	Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
 		rb = this.GetComponent<Rigidbody>();
-		
+	
+		offset = Camera.main.transform.position - this.transform.position;
+
 	}
 	
 	// Update is called once per frame
@@ -19,11 +23,13 @@ public class PlayerController : MonoBehaviour {
 		float hdir = Input.GetAxisRaw ("Horizontal");
 		float vdir = Input.GetAxisRaw ("Vertical");
 
+		Camera.main.transform.position = this.transform.position + offset;
+
 		Vector3 directionVector = new Vector3 (hdir, 0, vdir);
 		Vector3 unitVector = directionVector.normalized;
-		Vector3 forceVector = unitVector * moveSpeed + Time.deltaTime;
+		Vector3 forceVector = unitVector * moveSpeed * Time.deltaTime;
 
-		rb.AddForce(Vector3.forward * -10f);
+		rb.AddForce(forceVector);
 		
 	}
 }
